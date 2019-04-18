d0404e87296076c677e222bd43772ca411e60360
BintangSaputra <bintangcbs26@gmail.com>
2019-04-18 11:02:10 +0700

    improve button function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let number = "";
 let secondNumber = "";
 let operator = "";
 
-const num = num => {
+const onNumberPressed = num => {
   number += num;
   document.querySelector("#result").innerHTML = number;
 };
@@ -16,4 +16,28 @@ const ops = type => {
 const numberValue = () => {
   let viewValue = document.getElementById("viewer");
   document.querySelector("").innerHTML = "";
+
+  let total = 0;
+
+  const firstNumber = parseInt(number);
+  const secNumber = parseInt(secondNumber);
+
+  switch (operator) {
+    case "multiply":
+      total = firstNumber * secNumber;
+      break;
+    case "divide":
+      total = firstNumber / secNumber;
+      break;
+    case "add":
+      total = firstNumber + secNumber;
+      break;
+    case "substract":
+      total = firstNumber - secNumber;
+      break;
+    default:
+      break;
+  }
+
+  result.innerHTML = total;
 };
